e2dbdd7014e9e35b8de74e9bacfca08f75e33478
ntbaoheyhey <108352255+ntbaoheyhey@users.noreply.github.com>
2025-07-02 15:42:49 +0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,3 +1,16 @@
+`default_nettype none
+
+module tt_um_asiclab_example (
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output reg [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
 wire reset = ~rst_n;
 assign uio_out = 0;
 assign uio_oe = 0;
